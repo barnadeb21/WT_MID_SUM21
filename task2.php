600c8464df773a5be9219069545ecb670f6cb817
barnadeb21 <84661012+barnadeb21@users.noreply.github.com>
2021-06-06 17:27:37 +0600

    Add files via upload

---
+++ task2.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html>
+<head>
+<style>
+table, th, td {
+  border: 1.4px solid black;
+}
+</style>
+</head>
+<body>
+
+<h1><th>American International University-Bangladesh</th></h1>
+<hr>
+
+<h2><th>Web Programming  [Sec :C]</th></h2>
+
+<h3><th>MId Term Exam</th></h3>
+<table>
+  <tr >
+    <th rowspan="1"><a href="">Aiub</a>
+</th>
+    <th>American International University-Bangladesh<hr></th>
+    <th>
+      <ul>
+          <li>C</li>
+          <li>S</li>
+      </ul>
+    </th>
+  </tr>
+  <tr>
+    <td>DEPT</td>
+    <td>
+    	<form>
+          <input type="radio" id="cs" name="Dept" value="cs">
+          <label for="cs">CS</label>
+          <input type="radio" id="cse" name="Dept" value="cse">
+          <label for="cse">CSE</label>
+          <input type="radio" id="cis" name="Dept" value="cis">
+          <label for="cis">CIS</label>
+          <input type="radio" id="csse" name="Dept" value="csse">
+          <label for="csse">CSSE</label>
+
+      	</form>
+     </td>
+     <td></td>
+  </tr>
+  <tr>
+    <td></td>
+    <td><center><button>Submit</button></center></td>
+    <td></td>
+  </tr>
+</table>
+
+</body>
+</html>
