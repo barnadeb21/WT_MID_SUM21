df2594caa93e2fbdd9d930d3142f81939115fde7
barnadeb21 <84661012+barnadeb21@users.noreply.github.com>
2021-06-19 13:07:15 +0600

    Add files via upload

---
+++ lab3d.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Lab 3 Problem 4</title>
+</head>
+<body>
+    <?php
+      $name = "Barna";
+
+      if($name == "Barna" || $name == "Deb" || $name == "Bob") {
+          echo "hello";
+      } else {
+          echo "you are not recognized";
+      }
+    ?>
+
+
+</body>
+</html>
