4f773a8208fb50ffe2c0cffd332e6331be350867
barnadeb21 <84661012+barnadeb21@users.noreply.github.com>
2021-06-19 13:06:53 +0600

    Add files via upload

---
+++ lab3c.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Lab 3 Problem 3</title>
+</head>
+<body>
+    <?php
+        $length = 10;
+        $height = 10;
+
+        $perimeter = 2 * ( $length + $height );
+        $area = $length * $height;
+
+        echo "PERIMETER = " . $perimeter . "</br>";
+        echo "AREA = " . $area . "</br>";
+
+        
+        if($length == $width) 
+            {
+              echo "the shape is a square";
+            }
+    ?>
+</body>
+</html>
