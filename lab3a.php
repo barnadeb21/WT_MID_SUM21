30b1be6bdb4e3e98d16ca0947fe716adbf410efa
barnadeb21 <84661012+barnadeb21@users.noreply.github.com>
2021-06-19 13:05:25 +0600

    Add files via upload

---
+++ lab3a.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Lab 3 Problem 1</title>
+</head>
+<body>
+A quick brown <?php echo "<b>fox</b>";?> jumps over the lazy <?php echo "<b>dog</b>";?>
+</body>
+</html>
