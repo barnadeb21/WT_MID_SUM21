25eeafb4bcdb91a019b8fc34bc09fbf1048c483c
barnadeb21 <84661012+barnadeb21@users.noreply.github.com>
2021-06-19 13:07:42 +0600

    Add files via upload

---
+++ lab3e.php
@@ -0,0 +1,24 @@
+<?php
+$backColor="green";
+$fontColor="red";
+$imageWidth="80px";
+$imageHeight="80px";
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Lab 3 problem 5</title>
+</head>
+
+<body>
+
+<table>
+<tr><td><img src="https://upload.wikimedia.org/wikipedia/en/4/45/AIUB_whole_logo.png" height="<?php echo $imageHeight;?>" width="<?php echo $imageWidth;?>"></td>
+<td><h2>American International University - <span style="background-color:<?php echo $backColor;?> ; color:<?php echo $fontColor?>">
+Bangladesh</h2></span></td></tr>
+</table>
+
+</body>
+</html>
