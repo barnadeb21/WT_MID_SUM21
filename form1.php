c8b6cf03f772324bba033df014e16b27cd8225e5
barnadeb21 <84661012+barnadeb21@users.noreply.github.com>
2021-06-22 19:30:16 +0600

    Add files via upload

---
+++ form1.php
@@ -0,0 +1,101 @@
+<!DOCTYPE html>
+<html>
+<html lang="en">
+    
+<head>
+<meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<title>Gorent-A renting service</title>
+		
+    </head>
+	</head>
+    
+	<body>
+	
+	<div><h1> Feedback Widget </h1></div>
+    <hr>
+    
+    <form>
+
+	<div>
+	<div><label> Your Name </label></div>
+	<div><input type="text" name="Your_Name" id="Your_Name" placeholder="Type here" ></div>
+	</div>
+    <br>
+    
+	<div>
+	<div><label> Email </label></div>
+	<div><input type="text" name="Email" id="Email" placeholder="abc@gmail.com" ></div>
+	</div>
+    <br>
+
+	<div>
+	<div><label>Your Review</label></div>
+	<div><textarea name="Review" id="Review" placeholder="Type here"></textarea></div>
+	</div>
+
+    <p>Overall Experience</p>
+    <input type="radio" id="very Good" name="exp" value="Very Good">
+      <label for="very good">Very Good</label>
+
+
+      <input type="radio" id="good" name="exp" value="Good">
+      <label for="good">Good</label>
+
+      <input type="radio" id="fair" name="exp" value="Fair">
+      <label for="fair">Fair</label>
+
+      <input type="radio" id="poor" name="exp" value="Poor">
+      <label for="poor">Poor</label>
+
+    <p>Timely Response</p>
+    <input type="radio" id="very Good" name="res" value="Very Good">
+      <label for="very good">Very Good</label>
+
+      <input type="radio" id="good" name="res" value="Good">
+      <label for="good">Good</label>
+
+      <input type="radio" id="fair" name="res" value="Fair">
+      <label for="fair">Fair</label>
+
+      <input type="radio" id="poor" name="res" value="Poor">
+      <label for="poor">Poor</label>
+
+
+
+    <p>Our Support</p>
+
+    <input type="radio" id="very Good" name="sup" value="Very Good">
+      <label for="very good">Very Good</label>
+
+      <input type="radio" id="good" name="sup" value="Good">
+      <label for="good">Good</label>
+
+      <input type="radio" id="fair" name="sup" value="Fair">
+      <label for="fair">Fair</label>
+
+      <input type="radio" id="poor" name="sup" value="Poor">
+      <label for="poor">Poor</label>
+
+
+
+    <p>Overall Satisfaction</p>
+    <input type="radio" id="very Good" name="ovs" value="Very Good">
+      <label for="very good">Very Good</label>
+
+      <input type="radio" id="good" name="ovs" value="Good">
+      <label for="good">Good</label>
+
+      <input type="radio" id="fair" name="ovs" value="Fair">
+      <label for="fair">Fair</label>
+
+      <input type="radio" id="poor" name="ovs" value="Poor">
+      <label for="poor">Poor</label>
+    
+    <br><br>
+    <Div>
+	<div><input type="Submit" value="Confirm"></div>
+	</form>	
+	</div>
+</body>
+</html>
---
+++ form2.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html>
+<html lang="en">
+    
+<head>
+<meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<title>Gorent-A renting service</title>
+		
+    </head>
+	</head>
+    
+	<body>
+	
+	<div><h1> Contact Us  </h1></div>
+    <hr>
+    
+    <form>
+
+
+	<div><input type="text" name="Name" id="Name" placeholder="Name" ></div>
+    <br>
+    
+
+	<div><input type="text" name="email" id="Email" placeholder="Email" ></div>
+    <br>
+
+
+    <div><input type="text" name="subject" id="Subject" placeholder="Subject" ></div>
+    <br>
+
+	
+	<div><textarea name="message" id="Message" placeholder="Message"></textarea></div>
+    <br>
+
+
+    <div>
+	<div><input type="Submit" value ="Submit"></div>
+	</form>	
+	</div>
+</body>
+</html>
